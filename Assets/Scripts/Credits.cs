85fce7729f0c2be9bbb8142cddccee33468a34a2
marinaber03 <marinaguadalupe31@gmail.com>
2024-12-22 18:28:56 +0100

    efecto de sonido 'ganar'

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 public class Credits : MonoBehaviour
 {
     [SerializeField] GameObject creditsCanvas = null;
+    public GameObject musicadeFondo;
+    private AudioSource audioSource;
+    private AudioClip clip;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        audioSource.volume = 0.1f;
+
+        if (other.CompareTag("Player"))
         {
             creditsCanvas.SetActive(true);
+            musicadeFondo.GetComponent<AudioSource>().Stop();
+            audioSource.PlayOneShot(audioSource.clip);
         }
     }
 }
